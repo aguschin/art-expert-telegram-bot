84dbe01df3bb3a17605827ef28fbdf3f3a5ea707
Pavel Ignatovich <p.ignatovich97@gmail.com>
2023-04-13 22:50:12 +0400

    formatting

--- fastapi-deployment/lang_metric.py
+++ fastapi-deployment/lang_metric.py
@@ -1,23 +1,21 @@
-from prometheus_fastapi_instrumentator.metrics import Info
 import numpy as np
 from PIL import Image
-from prometheus_client import Counter, Histogram, Gauge
-
+from prometheus_client import Counter, Gauge, Histogram
+from prometheus_fastapi_instrumentator.metrics import Info
 
 RANDOM_VALUE = Gauge(
-    "random_value",
-    "Random value that is set to show monitoring works"
+    "random_value", "Random value that is set to show monitoring works"
 )
+
 
 def random_value(info: Info) -> None:
     RANDOM_VALUE.set(np.random.random())
 
 
-
 METRIC_EXAMPLE = Counter(
     "http_requested_languages_total",
     "Number of times a certain language has been requested.",
-    labelnames=("langs",)
+    labelnames=("langs",),
 )
 
 
@@ -32,8 +30,8 @@ def http_requested_languages_total(info: Info) -> None:
 
 
 MEAN_PIXEL_VALUE = Histogram(
-    'image_middle_pixel_value',
-    'Mean pixel value of the image',
+    "image_middle_pixel_value",
+    "Mean pixel value of the image",
     buckets=(0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275),
 )
 
@@ -44,6 +42,6 @@ async def image_middle_pixel_value(info: Info) -> None:
     # (limitations of `prometheus_fastapi_instrumentator`)
     # keeping the code piece as an example though
     form = await info.request.form()
-    im = Image.open(form['file'].file)
+    im = Image.open(form["file"].file)
     mean_value = np.array(im).mean()
     MEAN_PIXEL_VALUE.observe(mean_value)
