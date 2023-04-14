f249cb1084a140b2d3679a8dcf862a1c1caf464f
Pavel Ignatovich <p.ignatovich97@gmail.com>
2023-04-14 10:22:23 +0400

    Delpy bot

--- bot/bot/tg_bot.py
+++ bot/bot/tg_bot.py
@@ -1,20 +1,32 @@
-import os
+import random
+from io import BytesIO
 
-import numpy as np
 from coolname import generate
-from mlem.api import load
-from mlem.runtime.client import HTTPClient
+from httpx import AsyncClient
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import (
+    Application,
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
-# create a telegram bot and paste it here, or use `flyctl secrets set TELEGRAM_TOKEN=token` to set it secretly
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "YOUR_TOKEN")
-# add URL of you REST API app here
-client = HTTPClient(host="https://art-expert-fastapi.fly.dev", port=None)
+from bot.model_client import ModelClient
+from bot.settings import BotSettings
+
+settings = BotSettings()
+
+
+model_client = ModelClient(
+    model_api_url=settings.model_api_url,
+    httpx_client=AsyncClient(),
+)
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -30,18 +42,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def estimate_price(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.message.from_user
     photo_file = await update.message.photo[-1].get_file()
-    await photo_file.download_to_drive('user_photo.jpg')
+    file_io = BytesIO()
+    await photo_file.download_to_memory(out=file_io)
+    res = await model_client.predict(file_io)
 
-    res = client.predict("user_photo.jpg")
-
-    pic_name = " ".join(generate(np.random.randint(2, 5))).capitalize()
-    caption = f"""Original Title: {np.random.choice(["Unknown", pic_name])}
+    pic_name = " ".join(generate(random.randint(2, 5))).capitalize()
+    caption = f"""Original Title: {random.choice(["Unknown", pic_name])}
 Author: {{author}}
-Date: {np.random.choice(["2023", "Beginning of XXI century", "2020-es"])}
+Date: {random.choice(["2023", "Beginning of XXI century", "2020-es"])}
 Estimated price: {{price}} $ [Sothebys auction]
-Style: {np.random.choice(["Surrealism", "Realism", "Abstract Art", "Impressionism"])}
-Genre: {np.random.choice(["animal painting", "portrait", "abstract", "illustration", "sketch and study", "figurative", "landscape"])}
-Media: {np.random.choice(["oil", "pencil", "photo"])}
+Style: {random.choice(["Surrealism", "Realism", "Abstract Art", "Impressionism"])}
+Genre: {random.choice(["animal painting", "portrait", "abstract", "illustration", "sketch and study", "figurative", "landscape"])}
+Media: {random.choice(["oil", "pencil", "photo"])}
 Similar painting: https://www.sothebys.com/en/buy/fine-art/paintings/abstract/_eve-ackroyd-woman-as-still-life-4eb9
     """
     # Price is estimated using Sothebys.com data
@@ -50,20 +62,17 @@ Similar painting: https://www.sothebys.com/en/buy/fine-art/paintings/abstract/_e
     await update.message.reply_photo(
         update.message.photo[-1].file_id,
         caption=caption.format(
-            price=res["price"],
+            price=res.price,
             author=update.effective_user.full_name,
-        )
+        ),
     )
 
-def main():
-    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+def create_app() -> Application:
+    app = ApplicationBuilder().token(settings.telegram_token.get_secret_value()).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("hello", hello))
     app.add_handler(MessageHandler(filters.PHOTO, estimate_price))
 
-    app.run_polling()
-
-
-if __name__ == "__main__":
-    main()
+    return app
