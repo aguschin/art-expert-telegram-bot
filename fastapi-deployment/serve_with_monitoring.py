ae42a71832336cc8cf0965d383802bf21e94da7e
Pavel Ignatovich <p.ignatovich97@gmail.com>
2023-04-14 17:43:31 +0400

    Remove redundant metric

--- fastapi-deployment/serve_with_monitoring.py
+++ fastapi-deployment/serve_with_monitoring.py
@@ -10,7 +10,6 @@ def main():
         metrics=[
             "lang_metric.random_value",
             "lang_metric.http_requested_languages_total",
-            "lang_metric.image_middle_pixel_value",
         ]
     )
     server = FastAPIServer(
